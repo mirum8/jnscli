b93be92a032247b27a3f6b85bdaa374eb0d5ef3e
Rob Griazev <mirum8@gmail.com>
2026-04-30 12:48:00 +0500

    add splitOnFirst string helper

--- src/main/java/com/github/mirum8/jnscli/util/Strings.java
+++ src/main/java/com/github/mirum8/jnscli/util/Strings.java
@@ -7,4 +7,12 @@ public class Strings {
     public static boolean isJobNumber(String str) {
         return str.startsWith("%") && Integers.isInteger(str.substring(1));
     }
+
+    public static String[] splitOnFirst(String input, char delimiter) {
+        int idx = input.indexOf(delimiter);
+        if (idx < 0) {
+            return new String[]{input};
+        }
+        return new String[]{input.substring(0, idx), input.substring(idx + 1)};
+    }
 }
